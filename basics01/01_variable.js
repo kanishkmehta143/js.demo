7df74e183afc3e5e677c8d25a460b2268e5baa0a
Kanishk Mehta <128968662+kanishkmehta143@users.noreply.github.com>
2023-11-30 18:13:34 +0000

    Variable and constant

---
+++ basics01/01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 123456
+let accountEmail = "kanishk@gmail.com"
+var accountPassword = "12121212"
+accountCity = "Mumbai"
+
+
+accountEmail = "1kanishk@gmail.com"
+
+//accountId = 1234
+//constant value never change
+
+accountPassword = "131313"
+accountCity = "Nagpur"
+
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountPassword, accountCity])
